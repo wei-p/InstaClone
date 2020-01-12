8d0ac3a4f368a56c099a51e4b89fb9f728082393
Wei Pan <wp2251@columbia.edu>
2020-01-12 18:39:29 -0500

    auth and custom create form

--- Insta/views.py
+++ Insta/views.py
@@ -3,6 +3,9 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from Insta.models import Post
 
+from Insta.forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from Insta.models import Post
 
 class HelloWorld(TemplateView):
@@ -16,10 +19,12 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+# Login check must be the first class
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
+    login_url = 'login'
 
 class PostUpdateView(UpdateView):
     model = Post
@@ -30,4 +35,10 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('posts')
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    # 在同时要对database操作时，用reverse_lazy
+    success_url = reverse_lazy('login')
 
