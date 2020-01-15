3fc6fa5133b4d28843ee6175781552f721b6c13f
Wei Pan <wp2251@columbia.edu>
2020-01-15 15:24:12 -0500

    Important parts finished

--- Insta/models.py
+++ Insta/models.py
@@ -4,7 +4,51 @@ from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+        upload_to='static/images/profiles',
+        format='JPEG',
+        options={'quality': 100},
+        blank=True,
+        null=True
+    )
+
+    def get_connections(self):
+        connections = UserConnection.objects.filter(creator=self)
+        return connections
+
+    def get_followers(self):
+        followers = UserConnection.objects.filter(following=self)
+        return followers
+
+    def is_followed_by(self, user):
+        followers = UserConnection.objects.filter(following=self)
+        return followers.filter(creator=user).exists()
+
+
+class UserConnection(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    creator = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='friendship_creator_set'
+    )
+    following = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='friend_set'
+    )
+
+    def __str__(self):
+        return self.creator.username + ' follows ' + self.following.username
+
+
 class Post(models.Model):
+    author = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='my_posts'
+    )
     title = models.TextField(blank=True, null=True)
     image = ProcessedImageField(
         upload_to='static/images/posts',
@@ -17,11 +61,43 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("post_detail", args=[str(self.id)])
 
-class InstaUser(AbstractUser):
-    profile_pic = ProcessedImageField(
-        upload_to='static/images/profiles',
-        format='JPEG',
-        options={'quality': 100},
-        blank=True,
-        null=True
+    def get_like_count(self):
+        return self.likes.count()
+
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes'
     )
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+
+    class Meta:
+        unique_together = ('post', 'user')
+
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' likes ' + self.post.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    comment = models.CharField(max_length=100)
+    posted_on = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.comment
+
--- Insta/views.py
+++ Insta/views.py
@@ -1,12 +1,13 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+from annoying_master.annoying.decorators import ajax_request
 from Insta.models import Post
 
 from Insta.forms import CustomUserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from Insta.models import Post
+from Insta.models import Post, Like, InstaUser, UserConnection
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
@@ -14,6 +15,17 @@ class HelloWorld(TemplateView):
 class PostsView(ListView):
     model = Post
     template_name = 'index.html'
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return Post.objects.all()
+        current_user = self.request.user
+        following = set()
+        for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+            following.add(conn.following)
+        following.add(current_user)
+        return Post.objects.filter(author__in=following)
+
 
 class PostDetailView(DetailView):
     model = Post
@@ -36,9 +48,32 @@ class PostDeleteView(DeleteView):
     template_name = 'post_delete.html'
     success_url = reverse_lazy('posts')
 
+
+class UserDetailView(DetailView):
+    model = InstaUser
+    template_name = 'user_detail.html'
+
 class SignUp(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'signup.html'
     # 在同时要对database操作时，用reverse_lazy
     success_url = reverse_lazy('login')
+
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
 
