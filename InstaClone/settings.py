024cfd73e5a6056c27a88886217b32e9f485420f
Wei Pan <wp2251@columbia.edu>
2020-01-16 15:52:07 -0500

    Prepared for deployment

--- InstaClone/settings.py
+++ InstaClone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x(e-%x7m+4^7z6uqvxymtl5*+y3l9ye8$!b%mv&zih(moz!r!l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'Insta',
     'imagekit',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
